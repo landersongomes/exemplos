6b2ada227eb40a711ddd689fffc48c70cd1c82d7
Landerson Gomes dos Santos <landersongomes@users.noreply.github.com>
2018-05-02 17:42:07 -0300

    Update UBuscaIncremental.pas
    Alteração no código, correção e refactoring

--- Sentença_Busca_Incremental/UBuscaIncremental.pas
+++ Sentença_Busca_Incremental/UBuscaIncremental.pas
@@ -13,67 +13,54 @@ function SentencaBuscaIncremental(var aListaCampos, aComplWhere,
   aOrderBy: string; const aCampo, aOR_AND, aLimitador,
   aTexto: string): TStringList;
 var
-  lwhere, lCampos : TStringList;
-  ltexto : string;
+  lwhere, lCampos, lTexto : TStringList;
   I: Integer;
+  j: Integer;
 begin
   Result := TStringList.Create;
   Result.Clear;
-  ltexto := aTexto;
+  lTexto := TStringList.Create;
+  lTexto.Delimiter := aLimitador.Chars[0];
+  ltexto.DelimitedText := aTexto;
   lCampos := TStringList.Create;
   lCampos.Delimiter := ',';
   lCampos.DelimitedText := aCampo;
-
   for I := 0 to lCampos.Count -1 do
     begin
-
       Result.Append(aListaCampos) ;
       lwhere := TStringList.Create;
       lwhere.Clear;
 
-      while ltexto.IndexOf(aLimitador) >= 0 do
+      for j := 0 to lTexto.Count -1 do
         begin
           if lwhere.Text.IsEmpty then
-            begin
-              lwhere.Append(Format('where ((%s like %s) ',
-                  [lCampos[i], QuotedStr(Format( '%s%s%s',['%', Copy(ltexto, 0, ltexto.IndexOf(aLimitador)), '%']))]))
-            end
+            lwhere.Append(Format('where ((%s like %s) ',
+                  [lCampos[i], QuotedStr(Format( '%s%s%s',['%', lTexto[j] , '%']))]))
+
           else
-            begin
-              lwhere.Append(Format(' %s (%s like %s)',
-                [aOR_AND, lCampos[i], QuotedStr(Format( '%s%s%s',[ '%', Copy(ltexto, 0, ltexto.IndexOf(aLimitador)), '%']))]));
-            end;
-          ltexto := Copy(ltexto, ltexto.IndexOf(aLimitador)+1 + aLimitador.Length, lTexto.Length );
+            lwhere.Append(Format(' %s (%s like %s)',
+                [aOR_AND, lCampos[i], QuotedStr(Format( '%s%s%s',[ '%', lTexto[j], '%']))]));
+
         end;
 
       if lwhere.Text.IsEmpty then
+        lwhere.Append(Format('where ((%s like %s) ',
+              [lCampos[i], QuotedStr(Format( '%s%s%s',['%', lTexto.Text , '%']))]));
+
+      if (j = lTexto.Count) or (lTexto.Count = 0) then
         begin
-          lwhere.Append(Format('where ((%s like %s)', [lCampos[i], QuotedStr(Format( '%s%s%s',['%', ltexto, '%']))]));
-          lwhere.Add(aComplWhere);
-        end
-      else
-        begin
-          lwhere.Append(Format(' %s (%s like %s)', [aOR_AND, lCampos[i], QuotedStr(Format( '%s%s%s',['%', ltexto, '%']))]));
+          lwhere.Append(')');
           lwhere.Add(aComplWhere);
         end;
-
 
       Result.AddStrings(lwhere);
-
       if (i >= 0 ) and (i < (lCampos.Count -1)) then
-        begin
-          Result.Append('union');
-
-        end;
-
+        Result.Append('union');
       lwhere.Free;
-
     end;
-
   Result.Add(Format('%s', [aOrderBy]));
   lCampos.Free;
 
 end;
 
 end.
-
